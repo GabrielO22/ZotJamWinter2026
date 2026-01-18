1b9609dd99cc5f4d5d868512e1f955d45403b64a
lillyanav242 <130510219+lillyanav242@users.noreply.github.com>
2026-01-17 16:09:05 -0800

    Respawn/Checkpoint

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,37 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private Vector2 respawnPoint = new Vector2(0, 0);
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            rigidBody.linearVelocity = Vector2.zero;
+            transform.position = respawnPoint;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Checkpoint"))
+        {
+            respawnPoint = other.transform.position;
+            other.gameObject.SetActive(false);
+        }
+
+    }
+
+    
+}
+
